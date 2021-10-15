c4f0e51234c8a4df2f06d2ebdb2972d86e72f1cf
Souvik Maji <souvikmaji94@gmail.com>
2021-10-15 13:54:49 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (58.28%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,35 +1,16 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] res = new int[nums.length];
+        int i = 0, j = nums.length - 1;
         
-        int lastNeg = 0;
-        while(lastNeg < nums.length && nums[lastNeg] < 0){
-            lastNeg++;
-        }
-        
-        int firstPos = lastNeg;
-        lastNeg--;
-        
-        int k = 0;
-        while(lastNeg >= 0 && firstPos < nums.length) {
-            int x = (int)Math.pow(nums[lastNeg], 2);
-            int y = (int)Math.pow(nums[firstPos], 2);
-            
-            if(x < y){
-                res[k++] = x;
-                lastNeg --;
-            }else {
-                res[k++] = y;
-                firstPos ++;
+        for(int k = nums.length - 1; k >= 0 ;k --){
+            if(Math.abs(nums[i]) > Math.abs(nums[j])){
+                res[k] = (int)Math.pow(nums[i], 2);
+                i++;
+            } else {
+                res[k] = (int) Math.pow(nums[j], 2);
+                j--;
             }
-        }
-        
-        while(lastNeg >= 0){
-            res[k++] = (int)Math.pow(nums[lastNeg--], 2);;
-        }
-        
-        while(firstPos < nums.length){
-            res[k++] = (int)Math.pow(nums[firstPos++], 2);;
         }
         
         return res;        
