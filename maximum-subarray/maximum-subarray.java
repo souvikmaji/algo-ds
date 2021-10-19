40cc1cb8903d8bafa37c4ec600f3fabe3a4b757f
Souvik Maji <souvikmaji94@gmail.com>
2021-10-19 20:06:21 +0530

    Time: 2 ms (30.53%), Space: 90.4 MB (5.87%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -3,17 +3,9 @@ class Solution {
       
         int currentSum = nums[0], maxSum = nums[0];
         
-        for(int i = 1; i < nums.length; i++){
-            
-            if(nums[i] + currentSum > nums[i]){
-                currentSum += nums[i];
-            } else {
-             
-              currentSum = nums[i];  
-            }
-
-            maxSum = Math.max(maxSum, currentSum);
-            
+        for(int i = 1; i < nums.length; i++){ 
+            currentSum = Math.max(nums[i], nums[i] + currentSum);
+            maxSum = Math.max(maxSum, currentSum);     
         }
         
         return maxSum;
