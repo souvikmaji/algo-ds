da8b5092bcd0958139169414d09004cc9779df0e
Souvik Maji <souvikmaji94@gmail.com>
2021-11-19 10:48:58 +0530

    Time: 1 ms (95.33%), Space: 45.7 MB (82.48%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length -1;
+        while(i < j){
+            swap(s, i++, j--);
+            // i++;
+            // j--;
+        }
+    }
+    
+    private void swap(char[] s, int x, int y){
+        char temp = s[x];
+        s[x] = s[y];
+        s[y] = temp;
+    }
+}
